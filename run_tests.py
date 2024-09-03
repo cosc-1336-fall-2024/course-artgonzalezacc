5bb72f4f2e160b591ebc118a399e83ff3d451d52
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-09-03 22:36:29 +0000

    integer and float division examples

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,7 @@ the file in /tests/homework/b_in_proc_out/tests_in_proc_out
 has the test functions
 '''
 #from tests.homework.b_in_proc_out import tests_in_proc_out
-from tests.examples.a_example import tests_devprocess
+from tests.examples.b_input_process_output import tests_input_process_output
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_devprocess)
+suite = unittest.TestLoader().loadTestsFromModule(tests_input_process_output)
 unittest.TextTestRunner(verbosity=2).run(suite)
--- src/examples/b_input_proc_output/input_process_output.py
+++ src/examples/b_input_proc_output/input_process_output.py
@@ -5,3 +5,8 @@ def display_output():
 def test_config():
     return True
 
+def float_division(value1, value2): #parameters(variables)
+    return value1 / value2 #float division(decimals)
+
+def integer_division(value1, value2):
+    return value1 // value2 #integer (whole number division) doesn't include decimal portion
