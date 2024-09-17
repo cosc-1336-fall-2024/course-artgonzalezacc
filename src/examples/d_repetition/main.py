52cef6122a75c15b12b6e9a955a1d70009a601dd
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-09-17 21:48:15 +0000

    intro to while loop print hello

--- src/examples/d_repetition/main.py
+++ src/examples/d_repetition/main.py
@@ -1,2 +1,7 @@
 import repetition
 
+def main():
+    repetition.print_hello()
+
+main() #runs the program
+
--- src/examples/d_repetition/repetition.py
+++ src/examples/d_repetition/repetition.py
@@ -1,2 +1,10 @@
 def test_config():
     return True
+
+def print_hello():
+    num = 5
+    indx = 0
+
+    while (indx < num):
+        print(indx, (indx < num), 'hello')
+        indx = indx + 1#statement that makes indx < num FALSE
