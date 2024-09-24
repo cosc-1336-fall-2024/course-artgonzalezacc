5657942b95c82a4ea9e00a9313e87a1c88cdb937
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-09-24 22:43:14 +0000

    global constant?? variables

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -2,10 +2,8 @@
 import void_functions
 
 def main():
-    num1 = 5
-    print("main0", num1)
-    void_functions.local_and_params(num1)
-    print("main1", num1)
+    void_functions.hello_world()
+    void_functions.display_number()
     
 
 main()
--- src/examples/e_functions/void_functions.py
+++ src/examples/e_functions/void_functions.py
@@ -1,16 +1,25 @@
 #
+GLOBAL_INT = 100 #CONSTANT
+GLOBAL_INT = 1000 #honor system - it can be changed
+
 def hello_world():
+    global GLOBAL_INT #i want to override the global GLOBAL_INT VARIABLE
+    GLOBAL_INT = 5000 
+    print(GLOBAL_INT)
     print('Hello world!')
 
 def display_number():
     num = 5 #local variable
     print(num)
+    print(GLOBAL_INT)
 
 def show_numbers(num): #parameter(variable)
     num1 = 5 #local variable
     print("show_numbers", num, num1)
+    print(GLOBAL_INT)
 
 def local_and_params(num1):
     print("local_and_params0" ,num1)
     num1 = 10
     print("local_and_params1" ,num1)
+    print(GLOBAL_INT)
