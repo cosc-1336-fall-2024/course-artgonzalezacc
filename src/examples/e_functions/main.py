d8200f4e5f118c6f780c56edbfe54cb2239c94a9
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-09-24 22:25:28 +0000

    local and parameters not the same

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -4,8 +4,8 @@ import void_functions
 def main():
     num1 = 5
     print("main0", num1)
-    num1 = 10
-    void_functions.show_numbers(1)
+    void_functions.local_and_params(num1)
     print("main1", num1)
+    
 
 main()
--- src/examples/e_functions/void_functions.py
+++ src/examples/e_functions/void_functions.py
@@ -9,3 +9,8 @@ def display_number():
 def show_numbers(num): #parameter(variable)
     num1 = 5 #local variable
     print("show_numbers", num, num1)
+
+def local_and_params(num1):
+    print("local_and_params0" ,num1)
+    num1 = 10
+    print("local_and_params1" ,num1)
