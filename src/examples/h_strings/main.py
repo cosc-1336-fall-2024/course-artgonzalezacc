b2931f772d586e9caed369d9d345a8978f9c9993
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-10-01 22:36:40 +0000

    accessing chars in a string

--- src/examples/h_strings/main.py
+++ src/examples/h_strings/main.py
@@ -1,1 +1,7 @@
 #main program
+import strings
+
+def main():
+    strings.string_access_a_character()
+
+main()
--- src/examples/h_strings/strings.py
+++ src/examples/h_strings/strings.py
@@ -1,2 +1,14 @@
 def test_config():
     return True
+
+def string_access_a_character():
+    lang = "C++"
+
+    print(lang)
+
+    print(lang[0])
+    print(lang[1])
+    print(lang[2])
+
+    #lang[0] = 'c' cannot modify individual characters in a string 
+    
