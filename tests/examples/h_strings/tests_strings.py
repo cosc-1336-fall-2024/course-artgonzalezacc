605a8af63fa6acee7f264a968fe83b0541965fec
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-10-03 22:16:08 +0000

    string concatenation

--- tests/examples/h_strings/tests_strings.py
+++ tests/examples/h_strings/tests_strings.py
@@ -7,3 +7,9 @@ class Test_Config(unittest.TestCase):
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
+    def test_string_concatenation(self):
+        lang = "Python"
+        result = lang + " is easy to learn"
+
+        self.assertEqual(result, "Python is easy to learn")
+
