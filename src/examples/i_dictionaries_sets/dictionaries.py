0361d0b5b7e56e1c46d17dd7d7641020cb3aef0b
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-10-29 21:54:32 +0000

    create dictionary access values with keys

--- src/examples/i_dictionaries_sets/dictionaries.py
+++ src/examples/i_dictionaries_sets/dictionaries.py
@@ -1,2 +1,11 @@
 def test_config():
     return True
+
+def use_a_dictionary():
+    phonebook = {'555-1111':'Chris', '555-2222':'Katie', '555-3333':'Joanne'}
+    print(phonebook)
+
+    print(phonebook['555-2222'])
+    print(phonebook['555-1111'])     
+    print(phonebook['555-3333'])
+    #print(phonebook['555-1112']) generates a KeyError--key doesn't exist
--- src/examples/i_dictionaries_sets/main.py
+++ src/examples/i_dictionaries_sets/main.py
@@ -1,1 +1,7 @@
 #main program
+import dictionaries
+
+def main():
+    dictionaries.use_a_dictionary()
+
+main()
