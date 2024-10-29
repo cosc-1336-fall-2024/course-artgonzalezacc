e3b5d0c4960b32889707a57cb8c8a4119df0d551
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-10-29 22:07:05 +0000

    test add key value pair to dictionary

--- tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -7,3 +7,15 @@ class Test_Config(unittest.TestCase):
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
+    def test_add_key_value_pair_dictionary(self):
+        phonebook = {'555-1111':'Chris', '555-2222':'Katie', '555-3333':'Joanne'}
+        expected_phonebook = {'555-1111':'Chris', '555-2222':'Katie', '555-3333':'Joanne', \
+                              '555-4444':'Python'}
+    
+        key = '555-4444'
+        value = 'Python'
+
+        if key not in phonebook:
+            phonebook[key] = value
+
+        self.assertEqual(phonebook, expected_phonebook)
