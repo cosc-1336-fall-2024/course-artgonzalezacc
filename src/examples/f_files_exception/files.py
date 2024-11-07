ace1bc20c1d11b1897f9870724a748daa1aad931
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-11-07 22:44:08 +0000

    file write and read

--- src/examples/f_files_exception/files.py
+++ src/examples/f_files_exception/files.py
@@ -1,1 +1,19 @@
 #
+def write_to_file(file_name):
+    file = open(file_name, 'w')
+
+    file.write('John Locke\n')
+    file.write('David Hume\n')
+    file.write('Edmund Burke\n')
+
+    file.close()
+
+def read_from_file(file_name):
+    file = open(file_name, 'r')
+
+    file_lines = file.read()
+
+    file.close()
+
+    print(file_lines)
+
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,1 +1,9 @@
 #main program
+import files
+
+def main():
+    file_name = 'names.txt'
+    files.write_to_file(file_name)
+    files.read_from_file(file_name)
+
+main()
