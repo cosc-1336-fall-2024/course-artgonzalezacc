81ac67a4ff11c32b6a47bb85b5b296a9bc64210b
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2024-11-12 23:19:12 +0000

    exceptions in test cases

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,7 @@ the file in /tests/homework/b_in_proc_out/tests_in_proc_out
 has the test functions
 '''
 #from tests.examples.b_input_process_output import tests_input_process_output
-from tests.examples.i_dictionaries_sets import tests_dictionaries_and_sets
+from tests.examples.f_files_exception import tests_files_exception
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_dictionaries_and_sets)
+suite = unittest.TestLoader().loadTestsFromModule(tests_files_exception)
 unittest.TextTestRunner(verbosity=2).run(suite)
--- tests/examples/f_files_exception/tests_files_exception.py
+++ tests/examples/f_files_exception/tests_files_exception.py
@@ -7,3 +7,10 @@ class Test_Config(unittest.TestCase):
     def test_configuration(self):
         self.assertEqual(True, test_config())
 
+    def test_divide_by_zero(self):
+        num1 = 4
+        num2 = 0
+
+        with self.assertRaises(ZeroDivisionError):
+            num1 / num2
+
